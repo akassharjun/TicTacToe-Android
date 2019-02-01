8f922492d381a44b2b2041f513a529b8bd300d35
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2019-02-01 21:25:44 +0530

    Added win overview in the Game Over dialog.

--- app/src/main/java/com/github/zkashu/tictactoe/VSComputerActivity.java
+++ app/src/main/java/com/github/zkashu/tictactoe/VSComputerActivity.java
@@ -3,13 +3,17 @@ package com.github.zkashu.tictactoe;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 import androidx.appcompat.app.ActionBar;
@@ -18,13 +22,73 @@ import androidx.appcompat.app.AlertDialog.Builder;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class VSComputerActivity extends AppCompatActivity implements OnClickListener {
+public class Frag extends AppCompatActivity implements View.OnClickListener {
 	
 	TicTacToe ticTacToe;
 	
+	
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_frag);
+		
+		TextView tv = findViewById(R.id.header);
+		
+		tv.setText(getString(R.string.vs_computer_title));
+		
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		
+		setSupportActionBar(toolbar);
+		
+		Objects.requireNonNull(getSupportActionBar()).setTitle("");
+		
+		showTokenDialog();
+		
+		for (int i = 1; i < 10; i++) {
+			String name = "spot" + i;
+			int id = getResources().getIdentifier(name, "id", this.getPackageName());
+			findViewById(id).setOnClickListener(this::onClick);
+		}
+		
+		findViewById(R.id.back).setOnClickListener(v-> finish());
+		
+	}
+	
+	private void resetVisualBoard(){
+		for (int i = 1; i < 10; i++) {
+			String spot = "spot" + i;
+			int id = getResources().getIdentifier(spot, "id", this.getPackageName());
+			
+			ImageView imageView = findViewById(id);
+			
+			if (imageView.getDrawable() != null){
+				imageView.setImageDrawable(null);
+			}
+			
+		}
+	}
+	
+	private void fillSpot (int spot, char token) {
+		String pos = "spot" + spot;
+		int id = getResources().getIdentifier(pos, "id", this.getPackageName());
+		
+		
+		if (id != 0) {
+			ImageView spotImage = findViewById(id);
+			
+			if (token == 'O') {
+				spotImage.setImageResource(R.drawable.ic_default_token_o);
+			} else {
+				spotImage.setImageResource(R.drawable.ic_default_token_x);
+			}
+			
+		}
+		
+	}
+	
 	public void showTokenDialog () {
 		final View view = getLayoutInflater().inflate(R.layout.dialog_token, null);
-		AlertDialog tokenDialog = new Builder(VSComputerActivity.this).create();
+		AlertDialog tokenDialog = new Builder(Frag.this).create();
 		Objects.requireNonNull(tokenDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(0x00000000));
 		
 		ImageButton tokenO = view.findViewById(R.id.tokenO);
@@ -47,85 +111,9 @@ public class VSComputerActivity extends AppCompatActivity implements OnClickList
 		tokenDialog.show();
 	}
 	
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_vscomputer);
-		
-		
-		ActionBar actionBar = getSupportActionBar();
-		
-		if (actionBar != null) {
-			actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-			actionBar.setCustomView(R.layout.tb_layout);
-		}
-		
-		TextView tv = findViewById(R.id.header);
-		
-		tv.setText(getString(R.string.vs_computer_title));
-		
-		Toolbar toolbar = findViewById(R.id.toolbar);
-		
-		setSupportActionBar(toolbar);
-		
-		showTokenDialog();
-		
-		for (int i = 1; i < 10; i++) {
-			
-			String string = getResources().getResourceName(R.id.pos1);
-			
-			Log.d("Name", string);
-			
-			String spot = "pos" + i;
-			Log.d("Spot", spot);
-			
-			
-			int id = getResources().getIdentifier(spot, "id", this.getPackageName());
-			Log.d("ResID", String.valueOf(id));
-			findViewById(id).setOnClickListener(this);
-		}
-		
-	}
-	
-	@Override
-	public void onBackPressed () {
-		super.onBackPressed();
-		finish();
-	}
-	
-	private void fillSpot (int position, char token) {
-		String spot = "pos" + position;
-		int id = getResources().getIdentifier(spot, "id", this.getPackageName());
-		
-		
-		if (id != 0) {
-			ImageButton button = findViewById(id);
-			
-			if (token == 'O') {
-				button.setImageResource(R.drawable.ic_token_o);
-			} else {
-				button.setImageResource(R.drawable.ic_token_x);
-			}
-			
-		}
-		
-	}
-	
-	private void resetVisualBoard(){
-		for (int i = 1; i < 10; i++) {
-			String spot = "pos" + i;
-			int id = getResources().getIdentifier(spot, "id", this.getPackageName());
-			
-			ImageButton button = findViewById(id);
-			
-			button.setImageResource(R.drawable.ic_token_empty);
-			
-		}
-	}
-	
 	private void showGameOverDialog (char token) {
 		final View view = getLayoutInflater().inflate(R.layout.dialog_game_over, null);
-		AlertDialog tokenDialog = new Builder(VSComputerActivity.this).create();
+		AlertDialog tokenDialog = new Builder(Frag.this).create();
 		Objects.requireNonNull(tokenDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(0x00000000));
 		
 		TextView message = view.findViewById(R.id.message);
@@ -133,10 +121,41 @@ public class VSComputerActivity extends AppCompatActivity implements OnClickList
 		Button playAgain = view.findViewById(R.id.playAgain);
 		Button quit = view.findViewById(R.id.quit);
 		
+		for (int pos = 1; pos < 10; pos++) {
+			
+			String mainName = "spot" + pos;
+			int mainID = getResources().getIdentifier(mainName, "id", this.getPackageName());
+			
+			ImageView mainImageView = findViewById(mainID);
+			
+			String name = "pos" + pos;
+			int id = getResources().getIdentifier(name, "id", this.getPackageName());
+			
+			Log.d("ID", String.valueOf(id));
+			Log.d("ID NAME", name);
+			
+			ImageView imageView = view.findViewById(id);
+			
+			if (mainImageView.getDrawable() != null){
+				imageView.setImageDrawable(mainImageView.getDrawable());
+			}
+		}
+		
+		Log.d("TYPE", ticTacToe.getWinRow());
+		
+		
+		
+		
 		if (ticTacToe.playerToken == token) {
 			message.setText(getString(R.string.player_won));
+			int id = getResources().getIdentifier(ticTacToe.getWinRow().toLowerCase(), "id", this.getPackageName());
+			
+			view.findViewById(id).setVisibility(View.VISIBLE);
 		} else if (ticTacToe.computerToken == token) {
 			message.setText(getString(R.string.computer_won));
+			int id = getResources().getIdentifier(ticTacToe.getWinRow().toLowerCase(), "id", this.getPackageName());
+			
+			view.findViewById(id).setVisibility(View.VISIBLE);
 		} else {
 			message.setText(getString(R.string.draw));
 			
@@ -158,313 +177,53 @@ public class VSComputerActivity extends AppCompatActivity implements OnClickList
 		tokenDialog.show();
 	}
 	
+	private void gameTurn (int pos) {
+		
+		if (ticTacToe.isSpotFree(pos)) {
+			
+			// Player Move
+			ticTacToe.insertToken(pos, ticTacToe.playerToken);
+			fillSpot(pos, ticTacToe.playerToken);
+			
+			// checking if the player has won
+			if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
+				showGameOverDialog(ticTacToe.playerToken);
+				return;
+			}
+			
+			// Computer Move
+			
+			int move = ticTacToe.computerMove();
+			ticTacToe.insertToken(move, ticTacToe.computerToken);
+			fillSpot(move, ticTacToe.computerToken);
+			
+			// checking if the computer has won
+			if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
+				showGameOverDialog(ticTacToe.computerToken);
+			}
+		} else {
+			Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
+		}
+		
+		
+		if (ticTacToe.isBoardFull()) {
+			showGameOverDialog('-');
+		}
+	}
+	
 	@Override
 	public void onClick (View v) {
-		int position = Integer.valueOf(getResources().getResourceName(v.getId()).substring(34));
 		
-		switch (v.getId()) {
-			case R.id.pos1:
-				if (ticTacToe.isSpotFree(position)) {
-					
-					// Player Move
-					
-					ticTacToe.insertToken(position, ticTacToe.playerToken);
-					fillSpot(position, ticTacToe.playerToken);
-					
-					// checking if the player has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
-						showGameOverDialog(ticTacToe.playerToken);
-						break;
-					}
-					
-					// Computer Move
-					
-					int move = ticTacToe.computerMove();
-					ticTacToe.insertToken(move, ticTacToe.computerToken);
-					fillSpot(move, ticTacToe.computerToken);
-					
-					// checking if the computer has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
-						showGameOverDialog(ticTacToe.computerToken);
-					}
-				} else {
-					Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-				if (ticTacToe.isBoardFull()) {
-					showGameOverDialog('-');
-				}
-				
-				break;
+		for (int pos = 1; pos < 10; pos++) {
+			String name = "spot" + pos;
+			int id = getResources().getIdentifier(name, "id", this.getPackageName());
 			
-			case R.id.pos2:
-				
-				if (ticTacToe.isSpotFree(position)) {
-					
-					// Player Move
-					
-					ticTacToe.insertToken(position, ticTacToe.playerToken);
-					fillSpot(position, ticTacToe.playerToken);
-					
-					// checking if the player has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
-						showGameOverDialog(ticTacToe.playerToken);
-						break;
-					}
-					// Computer Move
-					
-					int move = ticTacToe.computerMove();
-					ticTacToe.insertToken(move, ticTacToe.computerToken);
-					fillSpot(move, ticTacToe.computerToken);
-					
-					// checking if the computer has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
-						showGameOverDialog(ticTacToe.computerToken);
-					}
-				} else {
-					Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-				if (ticTacToe.isBoardFull()) {
-					showGameOverDialog('-');
-				}
-				
+			if (v.getId() == id) {
+				gameTurn(pos);
 				break;
-			case R.id.pos3:
-				if (ticTacToe.isSpotFree(position)) {
-					
-					// Player Move
-					
-					ticTacToe.insertToken(position, ticTacToe.playerToken);
-					fillSpot(position, ticTacToe.playerToken);
-					
-					// checking if the player has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
-						showGameOverDialog(ticTacToe.playerToken);
-						break;
-					}
-					// Computer Move
-					
-					int move = ticTacToe.computerMove();
-					ticTacToe.insertToken(move, ticTacToe.computerToken);
-					fillSpot(move, ticTacToe.computerToken);
-					
-					// checking if the computer has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
-						showGameOverDialog(ticTacToe.computerToken);
-					}
-				} else {
-					Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-				if (ticTacToe.isBoardFull()) {
-					showGameOverDialog('-');
-				}
-				
-				break;
-			case R.id.pos4:
-				if (ticTacToe.isSpotFree(position)) {
-					
-					// Player Move
-					
-					ticTacToe.insertToken(position, ticTacToe.playerToken);
-					fillSpot(position, ticTacToe.playerToken);
-					
-					// checking if the player has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
-						showGameOverDialog(ticTacToe.playerToken);
-						break;
-					}
-					// Computer Move
-					
-					int move = ticTacToe.computerMove();
-					ticTacToe.insertToken(move, ticTacToe.computerToken);
-					fillSpot(move, ticTacToe.computerToken);
-					
-					// checking if the computer has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
-						showGameOverDialog(ticTacToe.computerToken);
-					}
-				} else {
-					Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-				if (ticTacToe.isBoardFull()) {
-					showGameOverDialog('-');
-				}
-				
-				break;
-			case R.id.pos5:
-				if (ticTacToe.isSpotFree(position)) {
-					
-					// Player Move
-					
-					ticTacToe.insertToken(position, ticTacToe.playerToken);
-					fillSpot(position, ticTacToe.playerToken);
-					
-					// checking if the player has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
-						showGameOverDialog(ticTacToe.playerToken);
-						break;
-					}
-					// Computer Move
-					
-					int move = ticTacToe.computerMove();
-					ticTacToe.insertToken(move, ticTacToe.computerToken);
-					fillSpot(move, ticTacToe.computerToken);
-					
-					// checking if the computer has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
-						showGameOverDialog(ticTacToe.computerToken);
-					}
-				} else {
-					Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-				if (ticTacToe.isBoardFull()) {
-					showGameOverDialog('-');
-				}
-				
-				break;
-			case R.id.pos6:
-				if (ticTacToe.isSpotFree(position)) {
-					
-					// Player Move
-					
-					ticTacToe.insertToken(position, ticTacToe.playerToken);
-					fillSpot(position, ticTacToe.playerToken);
-					
-					// checking if the player has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
-						showGameOverDialog(ticTacToe.playerToken);
-						break;
-					}
-					// Computer Move
-					
-					int move = ticTacToe.computerMove();
-					ticTacToe.insertToken(move, ticTacToe.computerToken);
-					fillSpot(move, ticTacToe.computerToken);
-					
-					// checking if the computer has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
-						showGameOverDialog(ticTacToe.computerToken);
-					}
-				} else {
-					Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-				if (ticTacToe.isBoardFull()) {
-					showGameOverDialog('-');
-				}
-				
-				break;
-			case R.id.pos7:
-				if (ticTacToe.isSpotFree(position)) {
-					
-					// Player Move
-					
-					ticTacToe.insertToken(position, ticTacToe.playerToken);
-					fillSpot(position, ticTacToe.playerToken);
-					
-					// checking if the player has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
-						showGameOverDialog(ticTacToe.playerToken);
-						break;
-					}
-					// Computer Move
-					
-					int move = ticTacToe.computerMove();
-					ticTacToe.insertToken(move, ticTacToe.computerToken);
-					fillSpot(move, ticTacToe.computerToken);
-					
-					// checking if the computer has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
-						showGameOverDialog(ticTacToe.computerToken);
-					}
-				} else {
-					Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-				if (ticTacToe.isBoardFull()) {
-					showGameOverDialog('-');
-				}
-				
-				break;
-			case R.id.pos8:
-				if (ticTacToe.isSpotFree(position)) {
-					
-					// Player Move
-					
-					ticTacToe.insertToken(position, ticTacToe.playerToken);
-					fillSpot(position, ticTacToe.playerToken);
-					
-					// checking if the player has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
-						showGameOverDialog(ticTacToe.playerToken);
-						break;
-					}
-					// Computer Move
-					
-					int move = ticTacToe.computerMove();
-					ticTacToe.insertToken(move, ticTacToe.computerToken);
-					fillSpot(move, ticTacToe.computerToken);
-					
-					// checking if the computer has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
-						showGameOverDialog(ticTacToe.computerToken);
-					}
-				} else {
-					Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-				if (ticTacToe.isBoardFull()) {
-					showGameOverDialog('-');
-				}
-				
-				break;
-			case R.id.pos9:
-				if (ticTacToe.isSpotFree(position)) {
-					
-					// Player Move
-					
-					ticTacToe.insertToken(position, ticTacToe.playerToken);
-					fillSpot(position, ticTacToe.playerToken);
-					
-					// checking if the player has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.playerToken)) {
-						showGameOverDialog(ticTacToe.playerToken);
-						break;
-					}
-					// Computer Move
-					
-					int move = ticTacToe.computerMove();
-					ticTacToe.insertToken(move, ticTacToe.computerToken);
-					fillSpot(move, ticTacToe.computerToken);
-					
-					// checking if the computer has won
-					if (ticTacToe.checkWin(ticTacToe.board, ticTacToe.computerToken)) {
-						showGameOverDialog(ticTacToe.computerToken);
-					}
-				} else {
-					Toast.makeText(this, "This spot isn't free.", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-				if (ticTacToe.isBoardFull()) {
-					showGameOverDialog('-');
-				}
-				
-				break;
-			default:
-				break;
+			}
+			
 		}
+		
 	}
 }
